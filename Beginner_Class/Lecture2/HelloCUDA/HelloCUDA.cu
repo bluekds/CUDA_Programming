18064b70e47a3457a3241e2c8db4f4e9e2ae0322
Duksu Kim <bluekds@koreatech.ac.kr>
2021-08-21 16:29:03 +0900

    update lecture 2

---
+++ Beginner_Class/Lecture2/HelloCUDA/HelloCUDA.cu
@@ -0,0 +1,18 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+__global__ void helloCUDA(void)
+{
+	printf("Hello CUDA from GPU!\n");
+}
+
+int main(void)
+{
+	printf("Hello GPU from CPU!\n");
+
+	helloCUDA <<<1, 10 >>> ();
+
+	return 0;
+}
