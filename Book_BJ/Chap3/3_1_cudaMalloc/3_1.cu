b88ca142c07054937615410e804752efad8e0287
Duksu Kim <bluekds@koreatech.ac.kr>
2022-02-13 15:55:01 +0900

    Start to write a book

---
+++ Book_BJ/Chap3/3_1_cudaMalloc/3_1.cu
@@ -0,0 +1,8 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+int main(void)
+{
+	int* dDataPtr;
+	cudaMalloc(&dDataPtr, sizeof(int) * 32);
+}
---
+++ Book_BJ/Chap3/3_2_deviceMemory/3_1.cu
@@ -0,0 +1,29 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+
+void checkDeviceMemory(void)
+{
+	size_t free, total;
+	cudaMemGetInfo(&free, &total); // 현재 사용가능한 device memory 크기와, 총 device 메모리 크기를 얻어오는 함수
+	printf("Device memory (free/total) = %lld/%lld bytes\n"
+		, free, total);
+}
+
+int main(void)
+{
+	int* dDataPtr;
+	cudaError_t errorCode ;
+
+	checkDeviceMemory();
+	errorCode = cudaMalloc(&dDataPtr, sizeof(int) * 10240); // device memory 할당
+	printf("cudaMalloc - %s\n", cudaGetErrorName(errorCode));
+	checkDeviceMemory();
+
+	errorCode = cudaMemset(dDataPtr, 0, sizeof(int) * 10240); // device memory 초기화
+	printf("cudaMemset - %s\n", cudaGetErrorName(errorCode));
+
+	errorCode = cudaFree(dDataPtr);
+	printf("cudaFree - %s\n", cudaGetErrorName(errorCode)); // device memory 해제
+	checkDeviceMemory();
+}
