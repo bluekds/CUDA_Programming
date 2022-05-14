58e8f3a04dabc84a80848f30df0a1bd6d7801362
Duksu Kim <bluekdct@gmail.com>
2022-05-14 21:36:37 +0900

    Update the serial algorithm

--- Etc/BigWar/Common/BigWar.h
+++ Etc/BigWar/Common/BigWar.h
@@ -2,15 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <cmath>
 
 #define RANGE_MIN (0)
 #define RANGE_MAX (1024*1024)
 #define	ARMY_DIMENSION 3
+#define EPSILON (10e-6)
 
 typedef unsigned int UINT;
 typedef float POS_TYPE;
 
 class Army {
+public:
+	static POS_TYPE dist(Army _A, Army _B) {
+		POS_TYPE d = 0;
+		for (int i = 0; i < ARMY_DIMENSION; i++) {
+			d += (_A.pos[i] - _B.pos[i]) * (_A.pos[i] - _B.pos[i]);
+		}
+		return sqrt(d);
+	}
 public:
 	Army() { memset(this->pos, 0, sizeof(POS_TYPE) * ARMY_DIMENSION); }
 	~Army(){}
@@ -30,4 +41,24 @@ public:
 		};
 #endif
 	};
+};
+
+class Pair {
+public:
+	UINT A, B;
+	POS_TYPE dist;
+public:
+	static bool compare(Pair _pair1, Pair _pair2) {
+		if (std::abs((POS_TYPE)(_pair1.dist - _pair2.dist)) < EPSILON) { // equidistant  
+			if (_pair1.A == _pair2.A)
+				return _pair1.B < _pair2.B;
+			return _pair1.A < _pair2.A;
+		}
+		return _pair1.dist < _pair2.dist;
+	}
+public:
+	bool operator >(Pair& ref) {return !compare(*this, ref);}
+	bool operator <(Pair& ref) {return compare(*this, ref);}
+	void set(UINT _a, UINT _b, POS_TYPE _dist) { A = _a; B = _b; dist = _dist; }
+	void print(void) { printf("(%u,%u): %f\n", A, B, dist); }
 };
--- Etc/BigWar/SerialAlgorithm/main.cpp
+++ Etc/BigWar/SerialAlgorithm/main.cpp
@@ -1,5 +1,11 @@
 #include "../Common/BigWar.h"
 #include "Team.h"
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+#define NUM_RESULTS 100
+#define LAST_PAIR (NUM_RESULTS-1)
 
 int main(int argc, char** argv)
 {
@@ -12,7 +18,7 @@ int main(int argc, char** argv)
 	UINT numArmies = 0;	
 	Team *teams[2];
 
-	for (int i = 0; i < 2; i++) {
+	for (int i = 0; i < 2; i++) { // for each team
 		fopen_s(&fp, argv[i+1], "rb");
 		if (fp == NULL) {
 			printf("Fail to read the file - %s\n", argv[i + 1]);
@@ -21,6 +27,35 @@ int main(int argc, char** argv)
 		fread_s(&numArmies, sizeof(UINT), sizeof(UINT), 1, fp);
 		printf("%s: %d armys\n", argv[i + 1], numArmies);
 		teams[i] = new Team(numArmies, fp);
+		teams[i]->loadTeam();
 		fclose(fp);
 	}
+
+	vector<Pair> result(NUM_RESULTS);
+	for (int i = 0; i < NUM_RESULTS; i++)
+		result[i].dist = RANGE_MAX;
+
+	Pair curPair;
+	for (int iA = 0; iA < teams[0]->numArmies; iA++) {
+		for (int iB = 0; iB < teams[1]->numArmies; iB++) {
+			curPair.set(iA, iB, Army::dist(teams[0]->armies[iA], teams[1]->armies[iB]));
+			if (result[LAST_PAIR] > curPair) {
+				result[LAST_PAIR] = curPair;
+				sort(result.begin(), result.end(), Pair::compare);
+			}
+		}
+	}
+
+	// Write the result
+	FILE* wfp = NULL;
+	fopen_s(&wfp, argv[3], "w");
+	if (wfp == NULL) {
+		printf("Fail to open the output file\n");
+		exit(2);
+	}
+	for (int i = 0; i < NUM_RESULTS; i++) {
+		result[i].print();
+		fprintf(wfp, "%d %d %.2f\n", result[i].A, result[i].B, result[i].dist);
+	}
+	fclose(wfp);
 }
